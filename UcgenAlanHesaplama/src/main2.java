15a4c42f2d4aa6d6283d251c1fe6fbcc7b9dfb72
ilker baltacı <ilkerballtaci@gmail.com>
2021-07-27 21:17:58 +0300

    Ucgen alan hesaplama tekrar commit

--- UcgenAlanHesaplama/src/main2.java
+++ UcgenAlanHesaplama/src/main2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class main2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int a,b,c;
